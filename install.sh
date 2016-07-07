8d835aecee5e88f97f85edb56c7e5dd8b8ad0a54
Megh Parikh <meghprkh@gmail.com>
2016-07-07 16:38:03 +0530

    Modify how the vapi and header file is generated to fix libgamepadtest build

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 mkdir -p ${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi
 
 install -m 0644                                            \
-    ${MESON_BUILD_ROOT}/src/libgamepad@sha/libgamepad.vapi \
+    ${MESON_BUILD_ROOT}/src/libgamepad.vapi \
     ${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi
