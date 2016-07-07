840ea23ca08198da237b4515a81975b25e9e0511
Megh Parikh <meghprkh@gmail.com>
2016-07-07 17:24:00 +0530

    Remove the need for an install script

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir -p ${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi
-
-install -m 0644                                            \
-    ${MESON_BUILD_ROOT}/src/libgamepad.vapi \
-    ${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi
